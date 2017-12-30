4a0637607d98a24aa327b309e36eb61c64cbb3d4
Jordan Camilletti <jordan.camilletti@gmail.com>
2017-12-29 18:58:15 -0600

    Update Main.java

--- Main.java
+++ Main.java
@@ -6,7 +6,7 @@ public class Main {
 	public static void showMap(char[][] grid){
 		for(int n1=0;n1<10;n1++){
 			for(int n2=0;n2<10;n2++){
-				System.out.print();
+				System.out.print(grid[n1][n2]);
 			}
 			System.out.print("\n");
 		}
@@ -17,5 +17,7 @@ public class Main {
 		for(int i=0;i<100;i++){
 			grid[i/10][i%10]='X';
 		}
+		grid[4][4]='O';
+		showMap(grid);
 	}
 }
