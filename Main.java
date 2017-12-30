3388f5a325b8f48c79e7d7873da81e44037b2bb7
Jordan Camilletti <jordan.camilletti@gmail.com>
2017-12-29 18:51:15 -0600

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,1 +1,21 @@
 /**/
+
+package main;
+
+public class Main {
+	public static void showMap(char[][] grid){
+		for(int n1=0;n1<10;n1++){
+			for(int n2=0;n2<10;n2++){
+				System.out.print();
+			}
+			System.out.print("\n");
+		}
+	}
+	
+	public static void main(String[] args){
+		char[][] grid=new char[10][10];
+		for(int i=0;i<100;i++){
+			grid[i/10][i%10]='X';
+		}
+	}
+}
