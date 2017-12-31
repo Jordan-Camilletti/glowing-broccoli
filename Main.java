66f9d687e3d4b8017633cf1421fd0974eeca73fb
Jordan Camilletti <jordan.camilletti@gmail.com>
2017-12-31 16:25:20 -0600

    Update Main.java

--- Main.java
+++ Main.java
@@ -11,9 +11,9 @@ public class Main {
 		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		//JLabel l=new JLabel("<html>abcd<br/>efgh<br/>ijkl</html>");
 		Grid map=new Grid();//TODO: add 'map' to label 'l' to put the grid onto the window
-		map.setEmpty(map.grid);
+		map.setEmpty();
 		//map.showMap(map.grid);
-		f.add(map.showMap(map.grid));
+		f.add(map.showMap());
 		f.pack();
 		f.setVisible(true);
 	}
