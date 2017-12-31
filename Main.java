bbf89219992d2bfc2c2382e0409d5bd9693386e0
Jordan Camilletti <jordan.camilletti@gmail.com>
2017-12-31 00:31:00 -0600

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,10 +2,19 @@
 
 package main;
 
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
 public class Main {
 	public static void main(String[] args){
+		JFrame f=new JFrame("frame");
+		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		JLabel l=new JLabel("<html>abcd<br/>efgh<br/>ijkl</html>");
 		Grid map=new Grid();
 		map.setEmpty(map.grid);
 		map.showMap(map.grid);
+		f.add(l);
+		f.pack();
+		f.setVisible(true);
 	}
 }
