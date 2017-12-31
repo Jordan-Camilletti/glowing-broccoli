b1eacc5d88beabf7221630c6191a839ef09a620a
Jordan Camilletti <jordan.camilletti@gmail.com>
2017-12-30 23:39:22 -0600

    Update Main.java

--- Main.java
+++ Main.java
@@ -3,24 +3,9 @@
 package main;
 
 public class Main {
-	/*public static void showMap(char[][] grid){
-		for(int n1=0;n1<9;n1++){
-			for(int n2=0;n2<9;n2++){
-				System.out.print(grid[n1][n2]);
-			}
-			System.out.print("\n");
-		}
-	}
-	public static char[][] setMapEmpty(char[][] grid){
-		for(int i=0;i<81;i++){
-			grid[i/9][i%9]='X';
-		}
-		grid[4][4]='O';
-		return(grid);
-	}*/
-	
 	public static void main(String[] args){
-		/*char[][] grid=new char[9][9];
-		showMap(grid);*/
+		Grid map=new Grid();
+		map.setEmpty(map.grid);
+		map.showMap(map.grid);
 	}
 }
