f277ab529e4ca0544ab8b52cb3eb5e6d5b2045f6
Jordan Camilletti <jordan.camilletti@gmail.com>
2017-12-30 23:20:11 -0600

    Update Grid.java

--- Grid.java
+++ Grid.java
@@ -1,1 +1,27 @@
 /**/
+
+package main;
+
+public class Grid {
+	public char[][] grid=new char[9][9];
+	public Grid(){
+		for(int i=0;i<81;i++){
+			grid[i/9][i%9]='X';
+		}
+		grid[4][4]='O';
+	}
+	public void showMap(char[][] grid){
+		for(int n1=0;n1<9;n1++){
+			for(int n2=0;n2<9;n2++){
+				System.out.print(grid[n1][n2]);
+			}
+			System.out.print("\n");
+		}
+	}
+	public void setEmpty(char[][] grid){
+		for(int i=0;i<81;i++){
+			grid[i/9][i%9]='X';
+		}
+		grid[4][4]='O';
+	}
+}
