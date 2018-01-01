b9777442249e4e3391635f966a21eddd2939c5d1
Jordan Camilletti <jordan.camilletti@gmail.com>
2017-12-31 18:32:01 -0600

    Update Main.java

--- Main.java
+++ Main.java
@@ -9,10 +9,8 @@ public class Main {
 	public static void main(String[] args){
 		JFrame f=new JFrame("frame");
 		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		//JLabel l=new JLabel("<html>abcd<br/>efgh<br/>ijkl</html>");
 		Grid map=new Grid();
 		map.setEmpty();
-		//map.showMap(map.grid);
 		f.add(map.showMap());
 		f.pack();
 		f.setVisible(true);
