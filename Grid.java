61e50896b7724ff65f34f4ce2ebf4cea8966ed17
Jordan Camilletti <jordan.camilletti@gmail.com>
2018-01-06 00:17:50 -0600

    Update Grid.java

--- Grid.java
+++ Grid.java
@@ -5,14 +5,14 @@ package main;
 import javax.swing.JLabel;
 
 public class Grid {
-	public char[][] grid=new char[10][10];//complete grid, not all is seen at once
+	public char[][] grid=new char[12][12];//complete grid, not all is seen at once. 2 outside rows are all #
 	public JLabel l=new JLabel();
 	public int[] player=new int[2];
 	public boolean gameOver=false;
 	public char playerSpot='O';//this is the spot the player is standing on
 	public Grid(){
 		for(int i=0;i<100;i++){//sets the grid as empty
-			if(i/10==0||i/10==9||i%10==0||i%10==9){
+			if(i/10<2||i/10>=8||i%10<2||i%10>=8){
 				this.grid[i/10][i%10]='#';
 			}else if(Math.random()>=0.9){
 				this.grid[i/10][i%10]='^';
@@ -46,22 +46,22 @@ public class Grid {
 		this.grid[this.player[0]][this.player[1]]=this.playerSpot;
 		switch(wrd.toUpperCase()){
 		case "U": 
-			if(this.player[0]>1){
+			if(this.player[0]>2){
 				this.player[0]-=1;
 			}
 			break;
 		case "D": 
-			if(this.player[0]<8){
+			if(this.player[0]<=6){
 				this.player[0]+=1;
 			}
 			break;
 		case "L": 
-			if(this.player[1]>1){
+			if(this.player[1]>2){
 				this.player[1]-=1;
 			}
 			break;
 		case "R": 
-			if(this.player[1]<8){
+			if(this.player[1]<=6){
 				this.player[1]+=1;
 			}
 			break;
