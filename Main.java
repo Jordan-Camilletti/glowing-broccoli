f2d7d3c610c819b7d1d76356414dcfc9b8be50ca
Jordan Camilletti <jordan.camilletti@gmail.com>
2018-01-06 23:47:07 -0600

    Update Main.java

--- Main.java
+++ Main.java
@@ -8,7 +8,7 @@ import javax.swing.JLabel;
 
 public class Main {
 	public static void main(String[] args){
-		int ATP=3;
+		int ATP=3,turn=0;;
 		JFrame f=new JFrame("frame");
 		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		Grid map=new Grid();
@@ -19,11 +19,13 @@ public class Main {
 		String movement;
 		while(!map.gameOver){//TODO: add win/lose and change this
 			movement=k.nextLine();
+			turn++;
 			ATP=3;
 			while(ATP>0){
 				map.update(movement);
 				ATP--;
 			}
+			System.out.println(turn);
 		}
 	}
 }
