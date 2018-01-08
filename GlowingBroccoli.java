3d0cf5dbff0dadbdc760258d49c6e2962888505c
Jordan Camilletti <jordan.camilletti@gmail.com>
2018-01-08 17:48:05 -0600

    Update GlowingBroccoli.java

--- GlowingBroccoli.java
+++ GlowingBroccoli.java
@@ -8,17 +8,23 @@ import javax.swing.JLabel;
 
 public class GlowingBroccoli{
 	public static void main(String[] args){
+		int ATP=3,turn=1;
 		JFrame f=new JFrame("frame");
 		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		Grid map=new Grid();
-		f.add(map.showMap());
+		f.add(map.showMap(ATP,turn));
 		f.pack();
 		f.setVisible(true);
 		Scanner k=new Scanner(System.in);
 		String movement;
-		while(!map.endGame()){//TODO: add win/lose and change this
-			movement=k.nextLine();
-			map.update(movement);
+		while(!map.gameOver){//TODO: add win/lose and change this
+			System.out.println("Turn " + turn);
+			while(ATP>0){
+				movement=k.nextLine();
+				ATP=map.update(movement,ATP,turn);
+			}
+			turn++;
+			ATP=3;
 		}
 	}
 }
