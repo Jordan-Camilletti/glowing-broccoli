bba5adf71ed83c9aee540cd0577aae189ba9e3b2
Jordan Camilletti <jordan.camilletti@gmail.com>
2018-01-08 12:12:14 -0600

    Update and rename Main.java to GlowingBroccoli.java

--- GlowingBroccoli.java
+++ GlowingBroccoli.java
@@ -1,30 +1,24 @@
 /**/
 
-package main;
+package glowingBroccoli;
 
 import java.util.Scanner;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 
-public class Main {
+public class GlowingBroccoli{
 	public static void main(String[] args){
-		int ATP=3,turn=1;
 		JFrame f=new JFrame("frame");
 		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		Grid map=new Grid();
-		f.add(map.showMap(ATP,turn));
+		f.add(map.showMap());
 		f.pack();
 		f.setVisible(true);
 		Scanner k=new Scanner(System.in);
 		String movement;
-		while(!map.gameOver){//TODO: add win/lose and change this
-			System.out.println("Turn " + turn);
-			while(ATP>0){
-				movement=k.nextLine();
-				ATP=map.update(movement,ATP);
-			}
-			turn++;
-			ATP=3;
+		while(!map.endGame()){//TODO: add win/lose and change this
+			movement=k.nextLine();
+			map.update(movement);
 		}
 	}
 }
