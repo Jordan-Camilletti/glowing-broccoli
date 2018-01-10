9c24f5175189675d1703b097e3d4e919e8f1fb02
Jordan Camilletti <jordan.camilletti@gmail.com>
2018-01-09 23:26:27 -0600

    Update Enemy.java

--- Enemy.java
+++ Enemy.java
@@ -1,1 +1,14 @@
 /**/
+
+package glowingBroccoli;
+
+import java.util.Random;
+
+public class Enemy {
+	private int[] location=new int[2];
+	public Enemy(){
+		Random rnd=new Random();
+		location[0]=rnd.nextInt(10)+2;//2 and 10 inclusive
+		location[1]=rnd.nextInt(10)+2;
+	}
+}
