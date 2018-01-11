44528e99a5db8e3c2ddf32784297a5095f066b09
Jordan Camilletti <jordan.camilletti@gmail.com>
2018-01-10 21:17:22 -0600

    Update Enemy.java

--- Enemy.java
+++ Enemy.java
@@ -5,10 +5,19 @@ package glowingBroccoli;
 import java.util.Random;
 
 public class Enemy {
-	private int[] location=new int[2];
-	public Enemy(){
+	private int[] loc=new int[2];//enemy location
+	private char enemySpot='O';
+	public Enemy(char[][] map){
 		Random rnd=new Random();
-		location[0]=rnd.nextInt(10)+2;//2 and 10 inclusive
-		location[1]=rnd.nextInt(10)+2;
+		loc[0]=2;
+		loc[1]=2;
+		while(map[loc[0]][loc[1]]=='B'||map[loc[0]][loc[1]]=='S'){
+			loc[0]=rnd.nextInt(9)+2;
+			loc[1]=rnd.nextInt(9)+2;
+		}
+		enemySpot=map[loc[0]][loc[1]];
+	}
+	public int[] getLoc(){
+		return(loc);
 	}
 }
