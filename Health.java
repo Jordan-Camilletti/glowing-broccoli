336c4d218642ce77c4b615be0e19d8c50c52028f
Jordan Camilletti <jordan.camilletti@gmail.com>
2018-01-23 14:41:33 -0600

    Update Health.java

--- Health.java
+++ Health.java
@@ -6,4 +6,10 @@ public class Health{
     private int[] loc=new int[2];
     private char healthSpot='O';
     private boolean used=false;
+    public Health(){
+        create(map);
+    }
+    public void create(char[][] map){
+        
+    }
 }
