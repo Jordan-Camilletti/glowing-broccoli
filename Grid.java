89b80627a0a1368c314457f42c7924a82b8f0ab8
Jordan Camilletti <jordan.camilletti@gmail.com>
2018-01-24 23:25:49 -0600

    Update Grid.java

--- Grid.java
+++ Grid.java
@@ -94,7 +94,10 @@ public class Grid {
 		playerSpot=grid[player[0]][player[1]];
 		if(playerSpot=='^') ATP--;
 		for(int n=0;n<heals.length;n++){
-			
+			grid[heals[n].getLoc()[0]][heals[n].getLoc()[1]]=heals[n].getSpot();
+			if(player[0]==heals[n].getLoc()[0] && ){
+				
+			}
 		}
 		for(int n=0;n<enemies.length;n++){
 			grid[enemies[n].getLoc()[0]][enemies[n].getLoc()[1]]=enemies[n].getSpot();
