5fa28282a462c6ec8230fe00953456bb5e157d1b
Jordan Camilletti <jordan.camilletti@gmail.com>
2018-02-13 23:02:05 -0600

    Update Health.java

--- src/glowingBroccoli/Health.java
+++ src/glowingBroccoli/Health.java
@@ -8,32 +8,4 @@ public class Health extends BasicUnit{
 	public Health(char[][] map) {
 		super(map);
 	}
-	
-	/*private int[] loc=new int[2];
-	private char healthSpot='O';
-	private boolean alive=true;
-	private Random rnd=new Random();
-	public Health(char[][] map){
-		create(map);
-	}
-	public void create(char[][] map){
-		alive=true;
-		do{
-			loc[0]=rnd.nextInt(6)+2;
-			loc[1]=rnd.nextInt(6)+2;
-		}while(map[loc[0]][loc[1]]=='B'||map[loc[0]][loc[1]]=='S'||map[loc[0]][loc[1]]=='#');
-		healthSpot=map[loc[0]][loc[1]];
-	}
-	public void use(){
-		alive=false;
-	}
-	public int[] getLoc(){
-		return(loc);
-	}
-	public char getSpot(){
-		return(healthSpot);
-	}
-	public boolean getAlive(){
-		return(alive);
-	}*/
 }
