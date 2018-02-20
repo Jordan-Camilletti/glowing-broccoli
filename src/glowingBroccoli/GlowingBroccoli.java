dbdf2eba61623d39fcbd7aca1ee5d0cf7c91fa26
Jordan Camilletti <jordan.camilletti@gmail.com>
2018-02-19 18:29:03 -0600

    changed variable syntax to remove minor errors

--- src/glowingBroccoli/GlowingBroccoli.java
+++ src/glowingBroccoli/GlowingBroccoli.java
@@ -7,15 +7,15 @@ import javax.swing.JFrame;
 
 public class GlowingBroccoli{
 	public static void main(String[] args){
+		int turn=1;
+		Scanner k=new Scanner(System.in);
+		String movement;
 		Grid map=new Grid();
 		JFrame f=new JFrame("frame");
 		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		f.add(map.showMap(turn));
 		f.pack();
 		f.setVisible(true);
-		Scanner k=new Scanner(System.in);
-		String movement;
-		int turn=1;
 		while(!map.gameOver()){
 			System.out.println("Turn " + turn);
 			movement=k.nextLine();
