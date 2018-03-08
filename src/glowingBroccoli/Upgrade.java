043d2eddffad69555f7a9ee44c65788783430a4e
Jordan Camilletti <jordan.camilletti@gmail.com>
2018-03-07 23:09:22 -0600

    More improvements for upgrade class

--- src/glowingBroccoli/Upgrade.java
+++ src/glowingBroccoli/Upgrade.java
@@ -3,7 +3,10 @@
 package glowingBroccoli;
 
 public class Upgrade extends BasicUnit{
-  public Upgrade(){
-    
-  }
+	public Upgrade(char[][] map){
+		super(map);
+	}
+	public int getPower(){
+		return(rnd.nextInt(4));
+	}//0==+5 health,1==+10 health,2==2 free clones,3==+3 ATP,4==+1 ATP every turn
 }
